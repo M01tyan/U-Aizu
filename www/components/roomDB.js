3db8d84a0acd49b186d7f9a97d7c11d96453b921
M01tyan <kanta01m.tyan@gmail.com>
2018-04-10 22:56:56 +0900

    18/04/10

---
+++ www/components/roomDB.js
@@ -0,0 +1,60 @@
+function executeQuery(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS RoomTable (roomid primary key, name, typeid , placeid, timeid, weekid, status, foreign key (typeid) references TypeTable(typeid), foreign key (placeid) references TimeTable(placeid), foreign key (timeid) references TimeTable(timeid), foreign key (weekid) references TimeTable(weekid))');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS TypeTable (typeid, name)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS PlaceTable (placeid, name)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS TimeTable (timeid, open, close)');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS WeekTable (weekid, openweek)');
+        tx.executeSql('INSERT INTO TypeTable VALUES (1, "講義室")');
+        tx.executeSql('INSERT INTO TypeTable VALUES (2, "演習室")');
+        tx.executeSql('INSERT INTO TypeTable VALUES (3, "実験室")');
+        tx.executeSql('INSERT INTO TypeTable VALUES (4, "会議室")');
+        tx.executeSql('INSERT INTO TypeTable VALUES (5, "ゼミ室")');
+        tx.executeSql('INSERT INTO TypeTable VALUES (6, "その他")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (1, "講義棟1F")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (2, "講義棟2F")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (3, "研究棟1F")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (4, "研究棟2F")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (5, "研究棟3F")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (6, "管理棟3F")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (7, "学生ホール3F")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (8, "体育館")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (9, "運動場")');
+        tx.executeSql('INSERT INTO PlaceTable VALUES (10, "UBIC")');
+        tx.executeSql('INSERT INTO TimeTable VALUES (1, "8:30", "18:00")');
+        tx.executeSql('INSERT INTO TimeTable VALUES (2, "8:30", "20:30")');
+        tx.executeSql('INSERT INTO TimeTable VALUES (3, "8:30", "23:59")');
+        tx.executeSql('INSERT INTO TimeTable VALUES (4, "8:30", "17:00")');
+        tx.executeSql('INSERT INTO TimeTable VALUES (5, "0:00", "23:59")');
+        tx.executeSql('INSERT INTO WeekTable VALUES (1, "平日")');
+        tx.executeSql('INSERT INTO WeekTable VALUES (2, "毎日")');
+        tx.executeSql('INSERT INTO WeekTable VALUES (3, "授業のみ")');
+        tx.executeSql('INSERT INTO RoomTable VALUES (1, "M1", 1, 2, 1, 1, "true")');
+}
+
+function queryDB(tx) {
+        tx.executeSql('SELECT * FROM RoomTable', [], querySuccess, errorCB);
+}
+
+function querySuccess(tx, results) {
+        var len = results.rows.length;
+        for (var i=0; i<len; i++){
+            document.writeln("row = " + i + " ID = " + results.rows.item(i).id + " Data = " + results.rows.item(i).name+"<br/>");
+        }        
+}
+
+    //Callback function when the transaction is failed.
+function errorCB(err) {
+        console.log("Error occured while executing SQL: "+err.code);
+}
+
+    // Callback function when the transaction is success.
+function successCB() {
+        var db = window.openDatabase("RoomDB", "1.0", "RoomDB", 200000);
+        //db.transaction(queryDB, errorCB);
+}
+   
+function createDB(){
+        var db = window.openDatabase("RoomDB", "1.0", "RoomDB", 200000);
+        db.transaction(executeQuery, errorCB, successCB);
+ 
+}
--- www/components/usecampus.js
+++ www/components/usecampus.js
@@ -1,12 +1,8 @@
 // This is a JavaScript file
-var lectureLists = new Array("M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9", "M10", "M11", "M12", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12", "Aud", "LTh");
-var exerciseLists = new Array("CALL1", "CALL2", "iLab1", "iLab2", "NRLA", "NRLB", "std1", "std2", "std3", "std4", "std5", "std6");
-var experimentLists = new Array("hdw1", "hdw2", "hdw3", "hdw4");
-var conferenceLists = new Array("114", "128", "212", "中会議室", "小会議室", "大会議室", "F1会議室", "N-Lounge", "S-Lounge");
-var seminarLists = new Array("sem06", "sem07", "sem08", "sem09", "sem10");
-var otherLists = new Array("UBIC", "Geek Dojo", "グローバルラウンジ", "武道場", "体育館(半面1)", "体育館(半面2)", "プール", "運動場(フィールド側)", "運動場(芝生側)", "テニスコートA1", "テニスコートA2", "テニスコートB1", "テニスコートB2");
-
+var lectureLists = new Array("M1", "M2", "M3", "M4");
+var exerciseLists = new Array("std1", "std2", "std3", "std4");
 function editSelects(event) {
+  console.log(event.target[0]);
   document.getElementById('choose-sel').setAttribute('modifier', event.target.value);
   switch (event.target.value) {
     case 'lecture':
@@ -43,7 +39,7 @@ function setUsedLists(lists) {
                                                   '<span class="list-item__title">' + lists[index] + '</span><span class="list-item__subtitle">On the Internet</span>' +
                                                 '</div>' +
                                                 '<div class="right">' +
-                                                  '<ons-icon size="25px" icon="ion-information-circled"></ons-icon>' +
+                                                  '<ons-icon size="25px" icon="ion-information-circled" onclick="showTemplateDialog()"></ons-icon>' +
                                                 '</div>' +
                                               '</ons-list-item>');
       },
@@ -55,3 +51,23 @@ function setUsedLists(lists) {
     };
    list.refresh();
 }
+
+//部屋の詳細を表示
+var showTemplateDialog = function() {
+  var dialog = document.getElementById('my-dialog');
+
+  if (dialog) {
+    dialog.show();
+  } else {
+    ons.createElement('roomDetail.html', { append: true })
+      .then(function(dialog) {
+        dialog.show();
+      });
+  }
+};
+
+var hideDialog = function(id) {
+  document
+    .getElementById(id)
+    .hide();
+};
