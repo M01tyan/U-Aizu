78663e9dc1c1b1d2af18cd157516bfd0e0e89f7e
M01tyan <kanta01m.tyan@gmail.com>
2018-04-21 14:07:45 +0900

    18/04/21

--- www/components/roomDB.js
+++ www/components/roomDB.js
@@ -23,49 +23,73 @@ function setDB(){
                   //保存に失敗した場合の処理
               });
 }
+
+/*********campusStatusの初期化**********/
+function campusStatus(start, end){
+  this.start = start;
+  this.end = end;
+}
 var campusStatus = new Array(70);
 for(i=0; i<70; i++){
   campusStatus[i] = new Array(10);
 }
+/*************ここまで****************/
+
 //シラバスを抜き出す
 function getSchedule(){
   var yobi= new Array("日","月","火","水","木","金","土");
   var date = new Date();
   var day = date.getDate();
   var week = date.getDay();
-  $.ajax({
-      type: 'GET',
-      url: 'https://csweb.u-aizu.ac.jp/campusweb/campussquare.do?_flowId=KHW0001300-flow',
-      datatype: 'html',
-      success: function(data){
-        data = data.split("<td width=\"60%\" class=\"kyuko-cal-heijitsu center\">"+day+"("+yobi[week]+")</td>");
-        data = data[1].split("<td width=\"60%\" class=\"kyuko-cal-heijitsu center\">"+(day+1)+"("+yobi[(week+1)%7]+")</td>");
-        data = data[0].split("<td valign=\"top\">");
-        //console.log(data[1]);
-        for(i=1; i<70; i++){
-          var data2 = data[i].split("<span class=\"kyuko-shi-jugyo\">");
-          for(j=0; j<data2.length; j++){
-            var data3 = data2[j].split("</span>");
-            campusStatus[i][j] = data3[0];
-            console.log(campusStatus[i][j]);
+  console.log(yobi[week]);
+  if(yobi[week] != "土" || yobi[week] != "日"){
+    $.ajax({
+        type: 'GET',
+        url: 'https://csweb.u-aizu.ac.jp/campusweb/campussquare.do?_flowId=KHW0001300-flow',
+        datatype: 'html',
+        success: function(data){
+          data = data.split("<td width=\"60%\" class=\"kyuko-cal-donichi center\">"+day+"("+yobi[week]+")</td>");
+          data = data[1].split("<td width=\"60%\" class=\"kyuko-cal-donichi center\">"+(day+1)+"("+yobi[(week+1)]+")</td>");
+          data = data[0].split("<td valign=\"top\">");
+          //console.log(data[1]);
+          for(i=1; i<70; i++){
+            var data2 = data[i].split("<span class=\"kyuko-shi-jugyo\">");
+            for(j=0; j<data2.length; j++){
+              var data3 = data2[j].split("</span>");
+              if(data3[0].match('<br>')) break;
+              var data4 = data3[0].split(/-|\s/, 3);
+              console.log(data4[0]);
+              //campusStatus[i][j].start = data4[0];
+              //campusStatus[i][j].end = data4[1];
+              //campusStatus[i][j].name = data4[2];
+              //console.log(campusStatus[i][j]);
+              //console.log(campusStatus[i][j]);
+            }
           }
+        },
+        error: function(err){
+          console.log(err);
         }
-      },
-      error: function(err){
-        console.log(err);
-      }
-  });
-}
-function openSystem(){
-  ref = window.open('https://csweb.u-aizu.ac.jp/campusweb/campussmart.do?locale=ja_JP', '_blank', 'toolbar=yes,location=no,toolbarposition=buttom,enableViewportScale=yes,hidden=yes');
-  ref.addEventListener('loadstop', function() {
-      ref.executeScript({
-        code: "var userName = document.querySelector('#LoginFormSimple input[name=userName]'); userName.value='s1240236'; var password=document.querySelector('#LoginFormSimple input[name=password]'); password.value='No.1runner'; document.querySelector('#LoginFormSimple button').click();"
-      }, function() {
-        setTimeout(getSchedule, 2000);
-        setTimeout(ref.close, 10000);
-      });
     });
-    return false;
-  //setTimeout(getSchedule, 10000);
+  } 
+}
+
+function openSystem(){
+  var date = new Date();
+  var day = date.getDate();
+  var week = date.getDay();
+  localStorage.setItem('beforeDate', JSON.stringify(1));
+  if(localStorage.getItem('beforeDate') != day && (week != '0' || week != 6)){
+    ref = window.open('https://csweb.u-aizu.ac.jp/campusweb/campussmart.do?locale=ja_JP', '_blank', 'toolbar=yes,location=no,toolbarposition=buttom,enableViewportScale=yes,hidden=yes');
+    ref.addEventListener('loadstop', function() {
+        ref.executeScript({
+          code: "var userName = document.querySelector('#LoginFormSimple input[name=userName]'); userName.value='s1240236'; var password=document.querySelector('#LoginFormSimple input[name=password]'); password.value='No.1runner'; document.querySelector('#LoginFormSimple button').click();"
+        }, function() {
+          setTimeout(getSchedule, 2000);
+          setTimeout(ref.close, 10000);
+          localStorage.setItem('beforeDate', JSON.stringify(day));
+        });
+      });
+      return false;
+  }
 }
