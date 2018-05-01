eb96d33d7d1b493de4b48e0f0962e5470f2546a6
M01tyan <kanta01m.tyan@gmail.com>
2018-05-01 17:19:27 +0900

    18/05/01

--- www/components/attendence.js
+++ www/components/attendence.js
@@ -8,38 +8,69 @@ var next = function() {
   var carousel = document.getElementById('carousel');
   carousel.next();
 };
-
-var viewbool = false;
+var humanitiesLists = new Array("HS01 哲学", "HS03 心理学", "HS04 言語学");
+var gymLists = new Array("PA01 体育実技１", "PA02 体育実技２", "PA03 体育実技３", "PA04 体育実技４");
 function viewList(listNum){
+  var viewbool;
   var displayList;
   switch(listNum){
     case 0:
       displayList = document.getElementById('hiddenListLiberal');
+      viewbool = displayList.style.display;
+      setSyllabusLists(humanitiesLists, 'humanities');
+      setSyllabusLists(gymLists, 'gym');
       break;
     case 1:
       displayList = document.getElementById('hiddenListEnglish');
+      viewbool = displayList.style.display;
       break;
     case 2:
       displayList = document.getElementById('hiddenListBase');
+      viewbool = displayList.style.display;
       break;
     case 3:
       displayList = document.getElementById('hiddenListExpert');
+      viewbool = displayList.style.display;
       break;
     case 4:
       displayList = document.getElementById('hiddenListExercise');
+      viewbool = displayList.style.display;
       break;
     case 5:
       displayList = document.getElementById('hiddenListTeacher');
+      viewbool = displayList.style.display;
       break;
     case 6:
       displayList = document.getElementById('hiddenListProject');
+      viewbool = displayList.style.display;
       break;
   }
-  if(viewbool == false){
+  if(viewbool == "none"){
     displayList.style.display="block";
-    viewbool = true;
   } else {
     displayList.style.display="none";
-    viewbool = false;
   }
 }
+
+
+function setSyllabusLists(lists, id){
+  var list = document.getElementById(id); 
+  if(list.length){
+    for(i=0; i<lists.length; i++){
+      var listItem = ons.createElement('<ons-list-item>' + lists[i] + '</ons-list-item>');
+      list.appendChild(listItem);
+    }
+  }
+}
+
+document.addEventListener('show', function(event){
+  if(event.target.id == "addCourse"){
+    var hiddenLists = new Array({id: "hiddenListLiberal", num: 2}, {id: "hiddenListEnglish", num: 6}, {id: "hiddenListBase", num: 5}, {id: "hiddenListExpert", num: 5}, {id: "hiddenListExercise", num: 1}, {id: "hiddenListTeacher", num: 1}, {id: "hiddenListProject", num: 1});
+    for(i=0; i<hiddenLists.length; i++){
+      var syllabusList = document.getElementById(hiddenLists[i].id);
+      for(j=0; j<hiddenLists[i].num; j++){
+        console.log(syllabusList.getElementsByTagName('ons-list-header'));
+      }
+    }    
+  }
+});
